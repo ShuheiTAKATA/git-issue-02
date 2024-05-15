1e69be7c2482d703f73a241d8e3459ab7d516191
Shuhei TAKATA <takata@pascaliaasia.com>
2024-05-15 21:38:09 +0900

    create class01

---
+++ class01.php
@@ -0,0 +1,21 @@
+<?php
+
+class Class01
+{
+	private string $name;
+	
+	public function __construct(string $name)
+	{
+		$this->name = $name;
+	}
+
+	public function say(): void
+	{
+		echo 'Hello world!;
+	}
+
+	public function substract(int $x, int $y): int
+	{
+		return $x - $y;
+	}
+}
